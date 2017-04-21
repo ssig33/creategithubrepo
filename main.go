1c9daa2476f018658a79a1e6b813c858c0f518db
ssig33 <mail@ssig33.com>
2017-04-21 10:16:10 +0900

    suppor create private repo.

--- main.go
+++ main.go
@@ -20,12 +20,14 @@ func getClient(token string) (*github.Client, context.Context) {
 	return github.NewClient(tc), ctx
 }
 
-func createRepo(nameFlag, token string) {
-	fmt.Println("Tring to create repository:", nameFlag)
+func createRepo(name, token string, private bool) {
+	fmt.Println("Tring to create repository:", name)
+	fmt.Println("Private:", private)
 	client, ctx := getClient(token)
 
 	repo := github.Repository{
-		Name: &nameFlag,
+		Name:    &name,
+		Private: &private,
 	}
 
 	client.Repositories.Create(ctx, "", &repo)
@@ -36,16 +38,19 @@ func createRepo(nameFlag, token string) {
 func main() {
 	token := os.Getenv("GITHUB_TOKEN")
 	if token != "" {
-		var nameFlag string
-		flag.StringVar(&nameFlag, "name", "blank", "string flag")
+		var name string
+		var private bool
+		flag.StringVar(&name, "name", "blank", "name of repository")
+		flag.BoolVar(&private, "private", false, "private mode?")
+		flag.BoolVar(&private, "p", false, "private mode?")
+		flag.Parse()
 
-		if nameFlag == "blank" {
+		if name == "blank" {
 			path, _ := os.Getwd()
 			arr := strings.Split(path, "/")
-			nameFlag = arr[len(arr)-1]
-			fmt.Println(nameFlag)
+			name = arr[len(arr)-1]
 		}
-		createRepo(nameFlag, token)
+		createRepo(name, token, private)
 	} else {
 		fmt.Println("Please set Environment Value: GITHUB_TOKEN")
 	}
